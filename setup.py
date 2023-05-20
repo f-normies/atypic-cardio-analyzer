aedc6b85c029fee3f455d0f1d6142529051f43fe
Zakharov Oleg <dooggest@gmail.com>
2023-05-20 19:51:49 +0300

    v0.1
    - Added basic functionality

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="atypic-cardio-analyzer",
+    version="0.1",
+    description='Library created to analyse atypical cardiomyocyte action potential',
+    author='Oleg Zakharov, Aleksei Aredov, Ilya Polusmak',
+    author_email='os.zakharov.04@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'dask',
+        'openpyxl',
+        'scipy',
+        'scikit-image'
+    ],
+)
